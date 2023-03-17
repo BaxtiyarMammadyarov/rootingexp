2d30797fc2b5d7ecd7c927b063b44b0a2b886089
Baxtiyar Mammadyarov <74107721+BaxtiyarMammadyarov@users.noreply.github.com>
2023-03-17 20:30:57 +0400

    start

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { useState, useEffect } from 'react'
 import './App.css';
 import { Routes, Route, Link } from 'react-router-dom';
-import Home from './componrnts/Home'
-
+import Home from './componrnts/home/Home'
+import Header from './componrnts/header/Header'
 function App() {
   const [data, setData] = useState();
 
@@ -20,9 +20,11 @@ function App() {
     <>
       <div className="App">
         <Link to="/">Home</Link>
+        <Link to="/header">header</Link>
       </div>
       <Routes>
         <Route path='/' element={<Home data = {data}/>} />
+        <Route path='/header' element={<Header data = {data}/>} />
       </Routes>
 
 
